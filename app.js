21040294540ffb4f414204f9c9398bc983c85ae4
NathanSOLSK <nsolcansky@gmail.com>
2025-11-15 08:28:23 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -2,9 +2,14 @@
     let health = 100;
     let hunger = 50;
     let happiness = 70;
-    let energy = 40;
+    let energy = 30;
+    
     let dayNumber = 0;
-    let timeOfDay = 0.0;
+    let timeOfDay = 0;
+
+    let speed = 0.01;
+
+    let gameIsRunning = true;
 
    
 
@@ -15,9 +20,22 @@
     document.getElementById("happiness").textContent = happiness;
     document.getElementById("energy").textContent = energy;
     document.getElementById("day-number").textContent = dayNumber;
-    document.getElementById("time-of-day").textContent = timeOfDay;
+    
+   
    
 }
+
+    function gameLoop() {
+            if (gameIsRunning) {
+                lowerStats()
+                feed()
+                
+                
+            }
+        }
+
+        
+
     function lowerStats() {
         decreaseHunger()
         decreaseHapiness()
@@ -28,78 +46,146 @@
     }
 
     function feed() {
-        hunger += 1;
+        if (!gameIsRunning) return;
+        hunger += 3;
+        energy += 1;
+        health += 1;
         render();
+        
     }
 
     function wash() {
-        happiness += 5;
-        energy += 5;
+         if (!gameIsRunning) return;
+        happiness += 3;
+        energy += 2;
+        health += 5;
         render();
+        
     }
 
+    function sleep() {
+         if (!gameIsRunning) return;
+        energy += 10;
+        health += 5;
+        render();
+        
+    }
 
-     document.querySelector('button[data-action="clean"]').addEventListener('click', () => {
-    startWashAnimation();        // animácia hneď
-    setTimeout(wash, 5000); // efekt po 5s
-});
+    function play() {
+        if (!gameIsRunning) return;
+        happiness += 5;
+        energy -= 3;
+        render();
+        
+    }
     
 
-   
+     function gameOver() {
+        let message = "";
+        
+    if (hunger <= 0) {
+        message = "Tvoj kamarat zomrel od hladu";
+    }
+    if (energy <= 0) {
+        message = "Tvoj kamarat zomrel od vycerpania";
+    }
+    if (health <= 0) {
+        message = "Tvoj kamarat zomrel na chorobu";
+    }
+    if (happiness <= 0) {
+        message = "Tvoj kamarat spachal sebevrazdu";
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
+    if (message !== "") {
+        document.getElementById("game-over-message").textContent = message;
+        gameIsRunning = false;
+    
+}
+     }
 
 
 
 
     function decreaseHunger() {
+    if (!gameIsRunning) return;
     if (hunger > 0) {    
         hunger -= 1;
-        render();
+       
     }
+     render();
+     gameOver()
 }
 
 
 setInterval(decreaseHunger, 5000);
 
 function decreaseHealth() {
+     if (!gameIsRunning) return;
     if (health > 0) {    
         health -= 1;
-        render();
+       
     }
+     render();
+     gameOver()
 }
 setInterval(decreaseHealth, 6000);
 
 function decreaseHapiness() {
+     if (!gameIsRunning) return;
     if (happiness > 0) {    
         happiness -= 1;
-        render();
+       
     }
+     render();
+     gameOver()
 }
 setInterval(decreaseHapiness, 6000);
 
 function decreaseEnergy() {
+     if (!gameIsRunning) return;
     if (energy > 0) {    
         energy -= 1;
-        render();
+        
+       
     }
+     render();
+     gameOver()
+   
 }
 setInterval(decreaseEnergy, 6000);
+
+function dayCounter() {
+    if (!gameIsRunning) return;
+     dayNumber += 1;
+
+     render()
+}
+setInterval(dayCounter, 300000)
+
+function updateTime() {
+    if (!gameIsRunning) return;
+    
+    timeOfDay += speed;
+
+    if (timeOfDay >= 24) timeOfDay = 0, dayCounter();
+
+      let hours = Math.floor(timeOfDay);
+     let minutes = Math.floor((timeOfDay - hours) * 60);
+
+     let formattedTime = (hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes);
+    
+    document.getElementById("time-of-day").textContent = formattedTime; 
+  }
+  setInterval(updateTime, 200);
+
+
+
+ 
 
 
 
 
 
 render()
+
 
